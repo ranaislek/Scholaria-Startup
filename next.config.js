bfb9f4064c5662341ca554d6e21a17053f32a6cc
louaibouzaher <mohamedlouaibouzaher@gmail.com>
2024-05-17 01:32:03 +0200

    Add Tauri Config

---
+++ next.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  output: "export",
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/home",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: "upgrade-insecure-requests",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#067DFF",
+      },
+    },
+  },
+  plugins: [],
+};
